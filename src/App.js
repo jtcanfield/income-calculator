ccef76a05abc33f8aad62057fa076865c15696c7
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-18 12:29:58 -0500

    add basic form:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    };
+  }
+
+  componentWillMount() {
+
+  }
+  componentDidUpdate(){
+
+  }
+
+  updateFromField(stateKey) {
+      return (event) => {
+      this.setState({[stateKey]: event.target.value});
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to a Calculator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className="enterForm" onSubmit={this.handleFormSubmit}>
+          <div className="form-group">
+            <h6>Before Tax Income:</h6>
+            <input type="username" onChange={this.updateFromField('username')} value={this.state.username} placeholder="user@gmail.org"/>
+          </div>
+          <div className="form-group">
+            <h6>Password:</h6>
+            <input type="password" onChange={this.updateFromField('password')} value={this.state.password} placeholder="********"/>
+          </div>
+          {this.state.message ? this.state.message : ""}<br/>
+          <div className="form-group pull-right">
+            <button className="btn btn-primary btn-lg" type="submit" onClick={event => this.login(event)}>Login</button>
+          </div>
+        </form>
       </div>
     );
   }
