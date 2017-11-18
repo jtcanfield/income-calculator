0662534f9c285b9937518543d178375447d2d045
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-18 12:32:55 -0500

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -30,14 +30,14 @@ class App extends Component {
         </header>
         <form className="enterForm" onSubmit={this.handleFormSubmit}>
           <div className="form-group">
-            <h6>Before Tax Income:</h6>
+            <h6>Before :</h6>
             <input type="username" onChange={this.updateFromField('username')} value={this.state.username} placeholder="user@gmail.org"/>
           </div>
           <div className="form-group">
             <h6>Password:</h6>
             <input type="password" onChange={this.updateFromField('password')} value={this.state.password} placeholder="********"/>
           </div>
-          {this.state.message ? this.state.message : ""}<br/>
+          <br/>
           <div className="form-group pull-right">
             <button className="btn btn-primary btn-lg" type="submit" onClick={event => this.login(event)}>Login</button>
           </div>
